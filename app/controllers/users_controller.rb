fcb7e4042b4ac2d2cd90c5cc52f6acb2a49a1e58
tomonori48 <taka8_jef@icloud.com>
2020-12-24 08:31:24 +0000

    Finish full-fledged-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user #新規作成後に、ログイン状態を保持する。
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  
+  attr_accessor :remember_token #リメンバートークンの仮想属性を作成する。
   before_save { self.email = email.downcase } #大文字を小文字にさせる。
   
   validates :name,  presence: true, length: { maximum: 50 } #存在性、50文字以内
@@ -9,4 +11,35 @@ class User < ApplicationRecord
   
   has_secure_password #パスワード用のコード
   validates :password, presence: true, length: { minimum: 6 } #存在性、最小6文字
+  
+  #渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost = 
+    if ActiveModel::SecurePassword.min_cost
+      BCrypt::Engine::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
+   BCrypt::Password.create(string, cost: cost)
+  end 
+  
+  #ランダムなトークンを返します。
+  def User.new_token
+     SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+     return false if remember_digest.nil?
+     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+   #ユーザーのログイン情報を破棄します。
+  def forget
+    update_attribute(:remember_digest, nil)
+  end 
 end
